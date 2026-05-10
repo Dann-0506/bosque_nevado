96ec5d80cd99c551b8af52a7bc904ba4ef57fbae
Daniel Landero <dlandero2005@outlook.com>
2026-05-09 20:24:18 -0600

    Enhance rendering: add star generation and improve celestial rendering in RendererAmbiente; update constants for particle and star configurations in Constantes

--- src/main/java/com/dan/animacion/render/RendererAmbiente.java
+++ src/main/java/com/dan/animacion/render/RendererAmbiente.java
@@ -6,8 +6,34 @@ import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.glu.GLU;
 import com.jogamp.opengl.glu.GLUquadric;
 
+import java.util.Random;
+
 public class RendererAmbiente {
-    private GLU glu = new GLU();
+    private final GLU glu = new GLU();
+
+    private final float[] estrellasX;
+    private final float[] estrellasY;
+    private final float[] estrellasZ;
+    private final float[] estrellasBrillo;
+
+    public RendererAmbiente() {
+        int n = Constantes.CANTIDAD_ESTRELLAS;
+        estrellasX = new float[n];
+        estrellasY = new float[n];
+        estrellasZ = new float[n];
+        estrellasBrillo = new float[n];
+
+        Random rand = new Random();
+        for (int i = 0; i < n; i++) {
+            float cosZenith = rand.nextFloat();
+            float sinZenith = (float) Math.sqrt(1.0 - cosZenith * cosZenith);
+            float azimuth = rand.nextFloat() * 2.0f * (float) Math.PI;
+            estrellasX[i] = sinZenith * (float) Math.cos(azimuth);
+            estrellasY[i] = cosZenith;
+            estrellasZ[i] = sinZenith * (float) Math.sin(azimuth);
+            estrellasBrillo[i] = 0.5f + rand.nextFloat() * 0.5f;
+        }
+    }
 
     public void prepararCielo(GL2 gl, CicloDiaNoche ciclo) {
         float[] cielo = ciclo.getColorCielo();
@@ -20,15 +46,17 @@ public class RendererAmbiente {
         float[] sol      = ciclo.getColorSol();
         float[] ambiente = ciclo.getColorAmbiente();
         float[] posSol   = ciclo.getPosicionSol();
+        float   solY     = ciclo.getSolY();
 
         gl.glLightfv(GL2.GL_LIGHT0, GL2.GL_POSITION, posSol, 0);
-        gl.glLightfv(GL2.GL_LIGHT0, GL2.GL_DIFFUSE,  new float[]{ sol[0],      sol[1],      sol[2],      1.0f }, 0);
+        gl.glLightfv(GL2.GL_LIGHT0, GL2.GL_DIFFUSE,  new float[]{ sol[0], sol[1], sol[2], 1.0f }, 0);
         gl.glLightfv(GL2.GL_LIGHT0, GL2.GL_AMBIENT,  new float[]{ ambiente[0], ambiente[1], ambiente[2], 1.0f }, 0);
 
-        dibujarAstros(gl, posSol, sol, camX, camY, camZ);
+        dibujarAstros(gl, posSol, sol, solY, camX, camY, camZ);
     }
 
-    private void dibujarAstros(GL2 gl, float[] posSol, float[] colorSol, float camX, float camY, float camZ) {
+    private void dibujarAstros(GL2 gl, float[] posSol, float[] colorSol, float solY,
+                                float camX, float camY, float camZ) {
         final float  distancia  = 400.0f;
         final double RADIO_SOL  = 10.0;
         final double HALO_CERCA = 13.5;
@@ -41,6 +69,8 @@ public class RendererAmbiente {
         gl.glEnable(GL2.GL_BLEND);
         gl.glBlendFunc(GL2.GL_SRC_ALPHA, GL2.GL_ONE_MINUS_SRC_ALPHA);
         gl.glDepthMask(false);
+
+        dibujarEstrellas(gl, solY, camX, camY, camZ);
 
         GLUquadric quadric = glu.gluNewQuadric();
 
@@ -74,5 +104,25 @@ public class RendererAmbiente {
         gl.glDisable(GL2.GL_BLEND);
         gl.glEnable(GL2.GL_FOG);
         gl.glEnable(GL2.GL_LIGHTING);
+    }
+
+    private void dibujarEstrellas(GL2 gl, float solY, float camX, float camY, float camZ) {
+        if (solY >= 0.0f) return;
+
+        float opacidad = Math.min(1.0f, -solY / 0.3f);
+        float r = Constantes.RADIO_ESTRELLAS;
+
+        gl.glEnable(GL2.GL_POINT_SMOOTH);
+        gl.glHint(GL2.GL_POINT_SMOOTH_HINT, GL2.GL_NICEST);
+        gl.glPointSize(Constantes.TAMANO_ESTRELLA);
+        gl.glBegin(GL2.GL_POINTS);
+        for (int i = 0; i < Constantes.CANTIDAD_ESTRELLAS; i++) {
+            gl.glColor4f(1.0f, 1.0f, 1.0f, opacidad * estrellasBrillo[i]);
+            gl.glVertex3f(camX + estrellasX[i] * r,
+                          camY + estrellasY[i] * r,
+                          camZ + estrellasZ[i] * r);
+        }
+        gl.glEnd();
+        gl.glDisable(GL2.GL_POINT_SMOOTH);
     }
 }
